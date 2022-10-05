aa98ede69b4afb04fe246e999e14e9f292368aee
MkWf <markdevw@gmail.com>
2022-10-05 14:24:45 -0400

    Building highlight start

--- index.ts
+++ index.ts
@@ -6,74 +6,170 @@
 
  import {
   Mesh,
-  VideoTexture,
+  MeshStandardMaterial,
+  Vector3,
+  Shape,
+  ExtrudeGeometry,
+  TextureLoader,
   MeshBasicMaterial,
-  FrontSide,
   PlaneGeometry
 } from 'three';
 
-import VIDEO_URL from "./BigBuckBunny.mp4";   
+import {LineSegmentsGeometry} from 'three/examples/jsm/lines/LineSegmentsGeometry';
+import {Line2} from 'three/examples/jsm/lines/Line2.js';
+import {LineMaterial} from 'three/examples/jsm/lines/LineMaterial.js';
+import UBI_ICON from './ubi-icon.png';
 import ThreeJSOverlayView from '@ubilabs/threejs-overlay-view';
 
 let map: google.maps.Map;
 
-const SCREEN_SIZE = [50, 25];
-const ALTITUDE_OFFSET = 3;
-const SCREEN_POSITION = {
-  lat: 42.331491,
-  lng: -71.070327,
-  altitude: SCREEN_SIZE[1] / 2 + ALTITUDE_OFFSET
-};
-const SCREEN_ROTATION = [Math.PI / 2, 0, Math.PI / 13];
-
 const mapOptions = {
-  tilt: 70,
-  heading: 180,
-  zoom: 19,
-  center: { lat: 42.331491, lng: -71.070327 },
+  center: {
+    lat: 53.55493986295417,
+    lng: 10.007137126703523
+  },
+  heading: 324.66666666666674,
+  tilt: 65.66666666666667,
+  zoom: 19.43375,
   mapId: "15431d2b469f209e",
-  //backgroundColor: 'transparent',
-  //disableDefaultUI: true,
-  //gestureHandling: "none",
-  //keyboardShortcuts: false,
+  disableDefaultUI: true,
+  backgroundColor: 'transparent',
+  gestureHandling: 'greedy',
 };
+
+const BUILDING_HEIGHT = 31;
+const BUILDING_LINE_COLOR = 0xffffff;
+const BUILDING_FILL_COLOR = 0x000000;
+const Z_FIGHTING_OFFSET = 0.001;
+const LOGO_SIZE = [16, 16];
+const LOGO_POSITION = {
+  lat: 53.55463986295417,
+  lng: 10.007317126703523,
+  altitude: BUILDING_HEIGHT + Z_FIGHTING_OFFSET
+};
+const LOGO_ROTATION_Z = Math.PI / 12;
+const COLOR_CHANGE_DURATION = 30; // completes one hue cycle in x seconds
 
 function initMap(): void {
   const mapDiv = document.getElementById("map") as HTMLElement;
   map = new google.maps.Map(mapDiv, mapOptions);
 
-  const overlay = new ThreeJSOverlayView({
-    lat: 42.331491, lng: -71.070327
-  })
+  const overlay = new ThreeJSOverlayView(mapOptions.center);
   overlay.setMap(map);
 
-  const scene = overlay.getScene();
-  const video = document.createElement('video');
-
-  video.src = VIDEO_URL;
-  video.loop = true;
-  video.muted = true;
-  video.autoplay = true;
-  video.load();
-  video.play();
-
-  const videoTexture = new VideoTexture(video);
-  const videoMaterial = new MeshBasicMaterial({
-    map: videoTexture,
-    side: FrontSide
-  });
-
-  const screenGeometry = new PlaneGeometry(...SCREEN_SIZE);
-  const screen = new Mesh(screenGeometry, videoMaterial);
-
-  overlay.latLngAltToVector3(SCREEN_POSITION, screen.position);
-  screen.rotation.order = 'ZYX';
-  screen.rotation.set(...SCREEN_ROTATION);
-
-  scene.add(screen);
-  overlay.update = () => overlay.requestRedraw();
+  initScene(overlay).then(() => overlay.requestRedraw());
 }
 
+async function initScene(overlay) {
+  const scene = overlay.getScene();
+
+  const wireframePath = [
+    [10.00787808793857, 53.554574397774715],
+    [10.006971374559072, 53.55444226566294],
+    [10.006565280581388, 53.55467172811441],
+    [10.006569569754523, 53.55471724470295],
+    [10.007768697370686, 53.554874083634],
+    [10.007848668422987, 53.554846301309745],
+    [10.007913475744536, 53.554604563663226]
+  ];
+  const points = wireframePath.map(([lng, lat]) =>
+    overlay.latLngAltToVector3({lat, lng})
+  );
+
+  const line = getWireframe(points);
+  scene.add(line);
+  scene.add(getBuilding(points));
+  scene.add(await getLogo(overlay));
+
+  overlay.update = () => {
+    const time = performance.now();
+
+    line.material.resolution.copy(overlay.getViewportSize());
+    line.material.color.setHSL(
+      ((time * 0.001) / COLOR_CHANGE_DURATION) % 1,
+      0.69,
+      0.5
+    );
+
+    overlay.requestRedraw();
+  };
+}
+
+function getWireframe(points) {
+  const positions = new Float32Array(18 * points.length).fill(0);
+
+  const offset = new Vector3(0, 0, BUILDING_HEIGHT);
+  const pointsTop = points.map(p => p.clone().add(offset));
+
+  for (let i = 0, n = points.length; i < n; i++) {
+    points[i].toArray(positions, 6 * i);
+    points[(i + 1) % n].toArray(positions, 6 * i + 3);
+  }
+
+  let topOffset = points.length * 6;
+  for (let i = 0, n = pointsTop.length; i < n; i++) {
+    pointsTop[i].toArray(positions, topOffset + 6 * i);
+    pointsTop[(i + 1) % n].toArray(positions, topOffset + 6 * i + 3);
+  }
+
+  let vertEdgeOffset = points.length * 12;
+  for (let i = 0; i < points.length; i++) {
+    const p = points[i];
+    const pTop = pointsTop[i];
+
+    p.toArray(positions, vertEdgeOffset + 6 * i);
+    pTop.toArray(positions, vertEdgeOffset + 6 * i + 3);
+  }
+
+  const lineGeometry = new LineSegmentsGeometry();
+  lineGeometry.instanceCount = 3 * points.length;
+  lineGeometry.setPositions(positions);
+  const lineMaterial = new LineMaterial({
+    color: BUILDING_LINE_COLOR,
+    linewidth: 3
+  });
+
+  const line = new Line2(lineGeometry, lineMaterial);
+  line.computeLineDistances();
+  return line;
+}
+
+function getBuilding(points) {
+  const buildingMaterial = new MeshStandardMaterial({
+    transparent: true,
+    opacity: 0.5,
+    color: BUILDING_FILL_COLOR
+  });
+
+  const buildingShape = new Shape();
+  points.forEach((p, i) => {
+    i === 0 ? buildingShape.moveTo(p.x, p.y) : buildingShape.lineTo(p.x, p.y);
+  });
+
+  const extrudeSettings = {
+    depth: BUILDING_HEIGHT,
+    bevelEnabled: false
+  };
+  const buildingGeometry = new ExtrudeGeometry(buildingShape, extrudeSettings);
+  return new Mesh(buildingGeometry, buildingMaterial);
+}
+
+function getLogo(overlay) {
+  return new Promise(resolve => {
+    const loader = new TextureLoader();
+    loader.load(UBI_ICON, texture => {
+      const logoGeometry = new PlaneGeometry(...LOGO_SIZE);
+      const logoMaterial = new MeshBasicMaterial({
+        map: texture,
+        transparent: true
+      });
+      const logo = new Mesh(logoGeometry, logoMaterial);
+      overlay.latLngAltToVector3(LOGO_POSITION, logo.position);
+      logo.rotateZ(LOGO_ROTATION_Z);
+      resolve(logo);
+    });
+  });
+}
 
 
 /*function initWebglOverlayView(map: google.maps.Map): void {
